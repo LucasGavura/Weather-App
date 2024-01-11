38f56ea99f0e3ab817f5fc223155213587c715c2
Lucas Gavura <lgavu074@uottawa.ca>
2024-01-10 22:46:54 -0500

    Added API key and formatted the UI

--- src/Code/App/WeatherApp.jsx
+++ src/Code/App/WeatherApp.jsx
@@ -14,24 +14,36 @@ import sunny_icon from '../Images/Sunny.png';
 import wind_icon from '../Images/Wind.png';
 
 const Weather = () => {
+
+    let api = "b9b195fcaa328638617ed17e0d4cb234";
+
+    const search = () => {
+        const query = document.getElementsByClassName("locationInput");
+        if(query[0].value==="") {
+            return 0;
+        }
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${query[0].value}&units=Metric&appid=${api}`;
+
+    }
+
     return (
         <div className='container'>
             <div className="top-bar">
                 <input type="text" className="locationInput" placeholder='Search'/>
-                <div className="search-icon">
+                <div className="search-icon" on onClick={()=>{search()}}>
                     <img src={search_icon} alt="" />
                 </div>
             </div>
             <div className="weather-image">
                 <img src={partlyCloudy_icon} alt="" />
             </div>
-            <div className="weather-temp">20C</div>
-            <div className="weather-location">OttawaC</div>
+            <div className="weather-temp">-50°C</div>
+            <div className="weather-location">Ottawa</div>
             <div className="data-container">
                 <div className="elem">
                     <img src={humidity_icon} alt="" className="icon" />
                     <div className="data">
-                        <div className="humidity">15%</div>
+                        <div className="wind">15%</div>
                         <div className="displayText">Humidity</div>
                     </div>
                 </div>
